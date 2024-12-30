0065de86437f0c6b9e3f4e4c932b8710fb8f10cb
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-12-29 21:25:29 -0400

    feat(auth): add authentication middleware

---
+++ src/auth/middlewares/authentication.middleware.ts
@@ -0,0 +1,24 @@
+import type { NextFunction, Request, Response } from 'express';
+
+import { ResponseError, handlerErrors } from '@/common/utils';
+import { JsonWebToken } from '@/auth/utils';
+
+export const authentication = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { authorization } = req.headers;
+
+    const token = authorization?.split(' ')[1];
+    if (!token) {
+      throw new ResponseError({
+        messages: ['Authorization token is missing. Please provide a valid token.'],
+        error: 'unauthorized',
+      });
+    }
+
+    const payloadData = await JsonWebToken.verifyToken(token);
+    req.body.payload = JsonWebToken.getPayloadData(payloadData);
+    next();
+  } catch (error) {
+    handlerErrors(res, error);
+  }
+};
