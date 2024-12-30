ff6b95efc2ae8199594f7272a4ce65fd54da3a62
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-12-29 22:32:35 -0400

    feat: add routes

---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import { ENDPOINTS } from '@/common/constants';
+import { AuthRoutes } from '@/auth/routes/auth.routes';
+import { UserRoutes } from '@/users/routes/user.routes';
+
+export class Routes {
+  public static get routes(): Router {
+    const router = Router();
+
+    // path to check server status
+    router.get(ENDPOINTS.HEALTH, (req, res) => {
+      res.status(200).json({});
+    });
+
+    router.use(AuthRoutes.routes);
+    router.use(UserRoutes.routes);
+
+    return router;
+  }
+}
